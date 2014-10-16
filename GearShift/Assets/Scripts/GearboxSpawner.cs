a98279422092babc119bbfaec3fbd6448ca1ef08
Michelle Guo <ruoyunguo@gmail.com>
2014-10-16 11:10:04 -0400

    More Gear Models + Slight GUI Change + Rusted Gear
    - More of Jimmie's models added to Models folder
    - GUI Change, remove border and add BG
    - Rusted Gear deletion functionality on turn added

--- GearShift/Assets/Scripts/GearboxSpawner.cs
+++ GearShift/Assets/Scripts/GearboxSpawner.cs
@@ -21,6 +21,7 @@ public class GearboxSpawner : MonoBehaviour {
 	void Update(){
 		for (int i = 3; i >= 0; i--) {
 			if(createdGears[i].layer == LayerMask.NameToLayer ("Default")){
+				createdGears[i].tag = "Placed Gear";
 				createdGears[i] = spawnGear(i);
 			}
 		}
