92d194a7065881dc0f7c3cdeaea27c4e3886cc76
Michelle Guo <ruoyunguo@gmail.com>
2014-10-22 21:57:17 -0400

    Refined Rusted Gear Behavior
    Based on the GearCounter within GearController now instead of the
    temporary tag-change solution.

--- GearShift/Assets/Scripts/Gear/GearBehaviors/RustedGearScript.cs
+++ GearShift/Assets/Scripts/Gear/GearBehaviors/RustedGearScript.cs
@@ -2,35 +2,52 @@
 using System.Collections;
 
 public class RustedGearScript : MonoBehaviour {
-	public int movesTillDeletion = 5;
-	public int numItemAdded = 0;
-	private int prevTotalTagged = 0;
+	public int maxMovesTillDeletion = 5;
+	private int prevTotalPlaced = 0;
+	private bool turnChecked = false;
+	GearShift.GearCounter gearCounter;
 
-	// Use this for initialization
 	void Start () {
-
+		// Load the Controller's Gear Counter Script
+		GameObject gearSpawner = GameObject.Find ("GearCounter");
+		gearCounter = gearSpawner.GetComponent<GearShift.GearCounter> ();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		// Only check if this is also a placed object
 		if (this.gameObject.layer == LayerMask.NameToLayer ("Default")) {
-			// Check if another object was placed from the gearbox, edit later to only deal with locked gears
-			int placedGears = GameObject.FindGameObjectsWithTag ("Placed Gear").Length;
-			if (placedGears == prevTotalTagged + 1)
-					numItemAdded++;
+			// Make sure to only count a change on one gear added
+			int placedGears = gearCounter.gearsPlaced;
+			if (placedGears == prevTotalPlaced + 1){
+				maxMovesTillDeletion--;
+				turnChecked = false;
 
-			//print("Number of items tagged: " + placedGears + ", num total added: " + numItemAdded );
-
-			// Must be able to account for things like mass number of deletions
-			prevTotalTagged = placedGears;
-			if (numItemAdded == movesTillDeletion) {
-					Destroy (this.gameObject);
+				// Break with increasing probability per turn passed
+				breakProbability();
 			}
+			prevTotalPlaced = placedGears;
 		}
 
 	}
 
-
+	void breakProbability(){
+		int probDel = Random.Range(0, 101);
+		// Only check once per object placed
+		if (!turnChecked) {
+			if (maxMovesTillDeletion ==  2) { // Only seems to delete here, check...
+				//print ("Probability at turn 3 = " + probDel);
+				if (probDel <= 15)
+					Destroy (this.gameObject);
+			} else if (maxMovesTillDeletion == 1) {
+				//print ("Probability at turn 4 = " + probDel);
+				if (probDel <= 40)
+					Destroy (this.gameObject);
+			} else if (maxMovesTillDeletion == 0) {
+				//print ("Broken on turn 5");
+				Destroy (this.gameObject);
+			}
+			turnChecked = true;
+		}
+	}
 	
 }
