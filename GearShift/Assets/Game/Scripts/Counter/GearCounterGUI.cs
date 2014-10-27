e01e923e5c1f1673cbcc091fca67665fae229a57
Michelle Guo <ruoyunguo@gmail.com>
2014-10-26 22:42:33 -0400

    Gear Destroy GUI Label
    GUI label to tell player how many gears they've destroyed so far. Will
    likely be replaced by a bar in the future.

---
+++ GearShift/Assets/Game/Scripts/Counter/GearCounterGUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GearCounterGUI : MonoBehaviour {
+	public GUIStyle style;
+	public Rect counterPos = new Rect(10, Screen.height/6, Screen.width-20, 100);
+	GearShift.GearCounter gearCounter;
+	string destroyText; 
+	// Use this for initialization
+	void Start () {
+		// Load the Controller's Gear Counter Script
+		GameObject gearSpawner = GameObject.Find ("GearCounter");
+		gearCounter = gearSpawner.GetComponent<GearShift.GearCounter> ();
+		style.normal.textColor = Color.white;
+		style.fontSize = 18;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		destroyText = "Gears Destroyed: " + gearCounter.gearsRemoved.ToString(); // not destroyed though?
+		counterPos = new Rect(10, Screen.height/6, Screen.width-20, 100);
+	}
+
+	void OnGUI(){
+		GUI.Label (counterPos, destroyText, style);
+	}
+}
