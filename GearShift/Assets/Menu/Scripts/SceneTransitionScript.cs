b501f753b511957edea8d72b48366b6f7c63fcdb
Katherine Cykes <katherinecykes@gmail.com>
2014-10-29 00:59:13 -0400

    Scene Transition Added to Menu!
    Clicking "Start Game" now takes the user to the newly added
    LayerGreatness Scene.

---
+++ GearShift/Assets/Menu/Scripts/SceneTransitionScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GearShift
+{
+	public class SceneTransitionScript : MonoBehaviour
+	{
+		public Object scene;
+
+		// Use this for initialization
+		protected void Start () {}
+		
+		// Update is called once per frame
+		protected void Update () {}
+
+		void OnMouseDown()
+		{
+			if (scene != null)
+			{ Application.LoadLevel((scene).name); }
+		}
+	}
+}
